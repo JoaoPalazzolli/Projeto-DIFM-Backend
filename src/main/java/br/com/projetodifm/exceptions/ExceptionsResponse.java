25615e70fb4d408a351eba8b2be9dc3273d5ffde
João Palazzolli <joaopedropalazzolli@gmail.com>
2023-12-23 15:19:20 -0300

    Adicionado a possibilidade de enviar uma lista de mensagens

--- src/main/java/br/com/projetodifm/exceptions/ExceptionsResponse.java
+++ src/main/java/br/com/projetodifm/exceptions/ExceptionsResponse.java
@@ -2,12 +2,15 @@ package br.com.projetodifm.exceptions;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ExceptionsResponse implements Serializable{
 
     private static final long serialVersionUID = 1L;
@@ -15,4 +18,5 @@ public class ExceptionsResponse implements Serializable{
     private LocalDateTime timestamp;
     private String message;
     private String details;
+    private Set<String> messages;
 }
